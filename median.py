c586c32c10e48c82aa0f3012b9964524331306a5
Matushan Yogaraj <dev@matushan.com>
2022-10-12 21:19:11 +0100

    Implement method to find median of a list

--- median.py
+++ median.py
@@ -10,3 +10,10 @@ while True:
     else:
         break
 print(numbers)
+sorted_numbers = sorted(numbers)
+middle_index = len(sorted_numbers) // 2
+if len(sorted_numbers) % 2 == 0:
+    median = (sorted_numbers[middle_index - 1] + sorted_numbers[middle_index]) / 2
+else:
+    median = sorted_numbers[middle_index]
+print(float(median))
